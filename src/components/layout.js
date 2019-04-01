c902793aa5982289a6d5574de714df7718367540
Devon Blandin <dblandin@gmail.com>
2019-04-01 00:28:22 -0400

    Update styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,10 @@ import React from "react"
 import { Header } from "../components/header"
 
 export default ({ children }) => (
-  <div
-    style={{ margin: `0 auto 3rem auto`, maxWidth: 1024, padding: `0 1rem` }}
-  >
-    <Header />
+  <>
+  <Header />
+  <div className="layout-wrapper">
     <div className="content">{children}</div>
   </div>
+  </>
 )
