c586d6da10dd2a84ea38243b0e39c1de457d8c45
Devon Blandin <dblandin@gmail.com>
2019-04-10 22:20:01 -0400

    Add <head> plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,15 @@
 import React from "react"
 
 import { Header } from "../components/header"
+import { Helmet } from "react-helmet"
 
 export default ({ children }) => (
   <>
+  <Helmet>
+    <meta charSet="utf-8" />
+    <title>Carmen Winant</title>
+    <link rel="canonical" href="https://carmenwinant.com" />
+  </Helmet>
   <Header />
   <div className="layout-wrapper">
     <div className="content">{children}</div>
